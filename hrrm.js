6cad84ec37edb494d68ea18df5f5f1102f0750aa
Attaporn Peungsook <aofattapon321@gmail.com>
2022-03-09 00:26:06 +0700

    for by geeksforgeeks

--- hrrm.js
+++ hrrm.js
@@ -5,6 +5,8 @@ let hrrn = []
 let sum_bt = 0;
 const c = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 const arrival = [], burst = [];
+let avgtt = 0;
+let avgwt = 0;
 
 
 function getData(){
@@ -12,7 +14,6 @@ function getData(){
 
    const arrivalString = document.getElementById("ArrivalTimes").value;
    const BurstString = document.getElementById("BurstTimes").value;
-
 
    const arrival = arrivalString.split(' ').map(function(item) {
       return parseInt(item, 10);
@@ -22,7 +23,8 @@ function getData(){
       return parseInt(item, 10);
    });
 
-   
+   console.log(arrival);
+   console.log(burst);
 
    // check length 
    const n = burst.reduce((sum,number) => {
@@ -36,49 +38,84 @@ function getData(){
          name: c[i],
          at: arrival[i],
          bt: burst[i],
-         completed: 0,
+         wt: 0,
+         tt: 0,
+         completed: 0
       }
    
       hrrn.push(temp);
    
       // Variable for sum of all Burst Times
       sum_bt += burst[i];
-      console.log(sum_bt);
    }
 
+   console.log(hrrn);
+
+   // sort by at 
    hrrn.sort((a, b) => {
       return a.at - b.at
    });
 
 
-   // main 
+   console.log(hrrn);
 
-   for(let t = hrrn[0].at; t < sum_bt; t++){
-      console.log(t);
-
-      // set lower limit to response ratio
-      let hrr = -9999;
+   // Response Ratio = (W + S)/S
+   for(let t = hrrn[0].at; t < sum_bt;){
+      
+      // set lower limit hrr 
+      let hrr = -9999; 
 
       // Response Ratio Variable
       let temp = 0; 
 
       // Variable to store next process selected
-      let loc = 0; 
+      let loc = 0;
+
       for(let i = 0; i < n; i++){
 
          // Checking if process has arrived and is Incomplete
          if(hrrn[i].at <= t && hrrn[i].completed != 1){
 
             // Calculating Response Ratio
-            temp = (hrrn[i].bt + (t - hrrn))
+            temp = (hrrn[i].bt + (t - hrrn.at)) / hrrn[i].bt;
 
+            // Checking for Highest Response Ratio
+            if(hrr < temp){
+
+               // Storing Response Ratio
+               hrr = temp;
+
+               // Storing Location
+               loc = i;
+            }
          }
       }
+      
+      // Updating time value
+      t += hrrn[loc].bt;
+
+      // Calculation of waiting time
+      hrrn[loc].wt = t - hrrn[loc].at - hrrn[loc].bt;
+
+      // Calculation of Turn Around Time
+      hrrn[loc].tt = t - hrrn[loc].at;
+
+      // Sum Turn Around Time for average
+      avgtt += hrrn[loc].tt;
+
+      // Calculation of Normalized Turn Around Time
+      hrrn[loc].ntt = (hrrn[loc].tt / hrrn[loc].bt);
 
 
+      // Updating Completion Status
+      hrrn[loc].completed = 1;
 
-
+      avgwt += hrrn[loc].wt;
+      
    }
+
+   console.log(hrrn);
+
 
 }
 
